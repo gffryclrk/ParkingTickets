4619ce51624634875ab73d8c9f177ff1b8281fbb
gffryclrk <mr@gffryclrk.com>
2018-03-28 19:40:45 -0400

    Updated download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#download some files
+# download some files
+# bash is confusing...
+# 
+# relevant: https://www.cyberciti.biz/faq/bash-concatenating-variables-with-string/
+# https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
 
 url="http://opendata.toronto.ca/revenue/parking/ticket/parking_tickets_"
 declare -a years=("2008" "2009" "2010" "2011" "2012" "2013" "2014" "2015" "2016" "2017" "2018")
